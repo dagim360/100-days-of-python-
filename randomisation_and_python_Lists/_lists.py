4ce4020476aa0eb7901ee90db17086bfd71732b6
dagim360 <68782461+dagim360@users.noreply.github.com>
2022-06-02 16:06:47 -0700

    commit it

---
+++ randomisation_and_python_Lists/_lists.py
@@ -0,0 +1,7 @@
+states_of_america = ["Delaware","pensilvenia","Utah", "new Mexico", "Tennesee", "Ohio", "oregon", "Arizona", "Alaska",
+"massachusetts", "South Dakota", "Montana", "Washington","Hawaii"
+]
+
+states_of_america[1] = "pincilvania"
+states_of_america.append("dagim land")
+print(states_of_america)
